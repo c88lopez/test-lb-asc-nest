9dcbf5d512bc29dcae73264c0cf591ebd894ec8f
Cristian Lopez <cristian@getrentcheck.com>
2024-10-18 17:26:42 -0300

    remove iam request

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, Logger } from '@nestjs/common';
-import { IAMClient, ListUsersCommand } from '@aws-sdk/client-iam';
+// import { IAMClient, ListUsersCommand } from '@aws-sdk/client-iam';
 
 @Injectable()
 export class AppService {
@@ -8,13 +8,13 @@ export class AppService {
   async getHello(): Promise<string> {
     this.logger.log('getHello()');
 
-    const iamClient = new IAMClient({ region: 'sa-east-1' });
+    // const iamClient = new IAMClient({ region: 'sa-east-1' });
 
-    const response = await iamClient.send(new ListUsersCommand());
+    // const response = await iamClient.send(new ListUsersCommand());
+    //
+    // const userList = response.Users;
 
-    const userList = response.Users;
-
-    this.logger.log('userList: ' + JSON.stringify(userList));
+    // this.logger.log('userList: ' + JSON.stringify(userList));
 
     for (let i = 0; i < 100; i++) {
       console.log(i);
