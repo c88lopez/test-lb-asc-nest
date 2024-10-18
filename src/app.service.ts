aedf84d95643c80a4f799b4406dba5a70ef3cd07
Cristian Lopez <cristian@getrentcheck.com>
2024-10-18 16:55:57 -0300

    log iam user list

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,20 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { IAMClient, ListUsersCommand } from '@aws-sdk/client-iam';
 
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
 
-  getHello(): string {
+  async getHello(): Promise<string> {
     this.logger.log('getHello()');
+
+    const iamClient = new IAMClient({ region: 'sa-east-1' });
+
+    const response = await iamClient.send(new ListUsersCommand());
+
+    const userList = response.Users;
+
+    this.logger.log('userList: ' + JSON.stringify(userList));
 
     return 'Hello World!';
   }
