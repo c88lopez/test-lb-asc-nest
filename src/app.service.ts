869eaf4ec6ebbac6bf0aa85b0774515de058f506
Cristian Lopez <cristian@getrentcheck.com>
2024-10-28 12:04:18 -0300

    remove comments

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,6 @@
 import { hostname } from 'node:os';
 
 import { Injectable, Logger } from '@nestjs/common';
-// import { IAMClient, ListUsersCommand } from '@aws-sdk/client-iam';
 
 @Injectable()
 export class AppService {
@@ -9,14 +8,6 @@ export class AppService {
 
   async getHello(loopCount: number): Promise<string> {
     this.logger.log('getHello()');
-
-    // const iamClient = new IAMClient({ region: 'sa-east-1' });
-
-    // const response = await iamClient.send(new ListUsersCommand());
-    //
-    // const userList = response.Users;
-
-    // this.logger.log('userList: ' + JSON.stringify(userList));
 
     for (let i = 0; i < loopCount; i++) {
       console.log(i);
