ae16bc2b816c9fcd7dfd8a510678831d49c51dd8
Cristian Lopez <cristian@getrentcheck.com>
2024-10-29 17:44:01 -0300

    fix test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getHello(@Query('loop-count') loopCount?: string): Promise<string> {
-    return this.appService.getHello(loopCount ? Number(loopCount) : 10);
+  getHello(): string {
+    return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,8 @@
-import { hostname } from 'node:os';
-
-import { Injectable, Logger } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  private readonly logger = new Logger(AppService.name);
-
-  async getHello(loopCount: number): Promise<string> {
-    this.logger.log('getHello()');
-
-    for (let i = 0; i < loopCount; i++) {
-      console.log(i);
-    }
-
-    return 'Hello World! ' + hostname();
+  getHello(): string {
+    return 'Hello World!';
   }
 }
