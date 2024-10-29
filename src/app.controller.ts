5fe43976ad858743cb6ff50aac64a4e9b0e51d4b
Cristian Lopez <cristian@getrentcheck.com>
2024-10-29 17:36:47 -0300

    fix test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getHello(@Query('loop-count') loopCount: string): Promise<string> {
+  async getHello(@Query('loop-count') loopCount?: string): Promise<string> {
     return this.appService.getHello(loopCount ? Number(loopCount) : 10);
   }
 }
